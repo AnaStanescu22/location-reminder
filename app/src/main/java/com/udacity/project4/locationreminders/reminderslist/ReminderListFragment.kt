750da8eaa3864cc6bed16eb60027243bd82ece4f
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-05-03 16:30:15 +0300

    Implement logout

--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,12 +1,16 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import com.udacity.project4.utils.setTitle
 import com.udacity.project4.utils.setup
@@ -71,7 +75,11 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
+                AuthUI.getInstance().signOut(requireContext())
+                    .addOnCompleteListener {
+                        val intent = Intent(activity, AuthenticationActivity::class.java)
+                        startActivity(intent)
+                    }
             }
         }
         return super.onOptionsItemSelected(item)
